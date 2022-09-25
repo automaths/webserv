5ab79945a633dd9776e2ecf1b76dfa45a9cb5a6c
nimrodds <nsartral@student.42.fr>
2022-09-25 19:45:51 +0200

    comments server class

--- webserv/server.hpp
+++ webserv/server.hpp
@@ -15,15 +15,7 @@ class Server {
     private:
     
     int _server_fd;
-    struct sockaddr_in _address;
-    // char *_test_cat_start = "HTTP/1.1 200 OK\nDate: Mon, 27 Jul 2009 12:28:53 GMT\nServer: Apache/2.2.14 (Win32)\nLast-Modified: Wed, 22 Jul 2009 19:15:56 GMT\nContent-Length: 88\nContent-Type: text/html\nConnection: Closed\n\n<html>\n<head>\n<meta http-equiv='Refresh' content='0; URL=https://http.cat/";
-    // char *_test_cat_end = "' />\n</head>\n</html>";
-    int _new_socket;
-    int _addrlen = sizeof(_address);
-    fd_set _sockSet;                  /* Set of socket descriptors for select() */
-    struct timeval _timeout;       /* Timeout for select() */
-    int    _i;
-    int    _result;
+    struct sockaddr_in _address;                  /* Set of socket descriptors for select() */      /* Timeout for select() */
     int _number_clients;
     int _client[1000];
 
@@ -41,28 +33,28 @@ class Server {
     struct sockaddr_in getAddr(){return _address;}
 
     void initing(){
-        // creating the socket
-        if ((_server_fd = socket(AF_INET, SOCK_STREAM, 0)) == 0)
+        if ((_server_fd = socket(AF_INET, SOCK_STREAM, 0)) == 0) // creating the socket
             throw SocketCreationException();
-        // configuring the socket model
-        _address.sin_family = AF_INET;
+        _address.sin_family = AF_INET; // socket configuration
         _address.sin_addr.s_addr = INADDR_ANY;
         _address.sin_port = htons( PORT );
-        memset(_address.sin_zero, '\0', sizeof _address.sin_zero);
-        // adding the configurations on the created socket 
+        memset(_address.sin_zero, '\0', sizeof _address.sin_zero); // applying configurations on the created socket
         if (bind(_server_fd, (struct sockaddr *)&_address, sizeof(_address)) < 0)
             throw BindException();
-        // opening to right port to receive requests
-        if (listen(_server_fd, 10) < 0)
+        if (listen(_server_fd, 10) < 0) // opening the socket to the port
             throw ListenException();
-        _number_clients = 0;
+        _number_clients = 0; // initing number of client socket to null
     }
 
     void execute(){
-        char *_test_cat_start = (char *)"HTTP/1.1 200 OK\nDate: Mon, 27 Jul 2009 12:28:53 GMT\nServer: Apache/2.2.14 (Win32)\nLast-Modified: Wed, 22 Jul 2009 19:15:56 GMT\nContent-Length: 88\nContent-Type: text/html\nConnection: Closed\n\n<html>\n<head>\n<meta http-equiv='Refresh' content='0; URL=https://http.cat/";
+        char *_test_cat_start = (char *)"HTTP/1.1 200 OK\nDate: Mon, 27 Jul 2022 12:28:53 GMT\nServer: Apache/2.2.14 (Win32)\nLast-Modified: Wed, 22 Jul 2022 19:15:56 GMT\nContent-Length: 88\nContent-Type: text/html\nConnection: Closed\n\n<html>\n<head>\n<meta http-equiv='Refresh' content='0; URL=https://http.cat/";
         char *_test_cat_end = (char *)"' />\n</head>\n</html>";
-        int size_read;
-        int k;
+        int tmp;
+        int result;
+        int addrlen = sizeof(_address);
+        struct timeval timeout;
+        fd_set _sockSet; // Set of socket descriptors for select
+
         while (1)
         {
             // the fd_set needs to be configured for each call to select
@@ -70,20 +62,20 @@ class Server {
             FD_SET(_server_fd, &_sockSet); // put server socket to fd_set
             for (int i = 0; i < _number_clients; i++) 
                 FD_SET(_client[i], &_sockSet); // put each client socket to the fd_set
-            _timeout.tv_sec = 1; _timeout.tv_usec = 0; // set timeout
+            timeout.tv_sec = 1; timeout.tv_usec = 0; // set timeout
 
-            _result = select(10, &_sockSet, NULL, NULL, &_timeout); // max 10 requests, only checking reads
+            result = select(10, &_sockSet, NULL, NULL, &timeout); // max 10 requests, only checking reads
 
-            if (_result == -1)
+            if (result == -1)
                 throw SelectException(); 
-            else if(_result > 0) // number of sockets ready
+            else if(result > 0) // number of sockets ready
             {
                 if (FD_ISSET(_server_fd, &_sockSet)) // test the server socket to see if there is incoming connection
                 {
-                    if ((_new_socket = accept(_server_fd, (struct sockaddr *)&_address, (socklen_t*)&_addrlen)) < 0)
+                    if ((tmp = accept(_server_fd, (struct sockaddr *)&_address, (socklen_t*)&addrlen)) < 0)
                         throw AcceptException();
                     else
-                        _client[_number_clients++] = _new_socket;
+                        _client[_number_clients++] = tmp;
                 }
                 // test all client sockets to see which ones are ready
                 for (int i = 0; i < _number_clients; i++)
@@ -93,18 +85,17 @@ class Server {
                         if (read( _client[i] , buffer, 10000) == -1)
                             throw ReadException();
                         printf("%s\n",buffer ); // request to parse and give a response to 
-                        k = 0;
+                        tmp = 0;
                         if (write(_client[i] , _test_cat_start , strlen(_test_cat_start)) == -1) // the request response
                             throw WriteException();
-                        while (buffer[k + 5] >= '0' && buffer[k + 5] <= '9')
-                            if (write(_client[i], &buffer[k++ + 5], 1))
+                        while (buffer[tmp + 5] >= '0' && buffer[tmp + 5] <= '9')
+                            if (write(_client[i], &buffer[tmp++ + 5], 1))
                                 throw WriteException();
                         if (write(_client[i] , _test_cat_end , strlen(_test_cat_end)))
                             throw WriteException();
                         close (_client[i]);
                         _client[i] = -1;
                     }
-
             }
             printf("The number of clients is %d\n", _number_clients);
         }
