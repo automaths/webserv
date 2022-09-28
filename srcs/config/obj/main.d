186aeebbe8176206e696518c2750078cbc645eb3
nimrodds <nsartral@student.42.fr>
2022-09-28 15:28:03 +0200

    http block

---
+++ srcs/config/obj/main.d
@@ -0,0 +1,12 @@
+obj/main.o: main.cpp ../library.hpp ../Request.hpp ../server.hpp \
+  ../Client.hpp config.hpp
+
+../library.hpp:
+
+../Request.hpp:
+
+../server.hpp:
+
+../Client.hpp:
+
+config.hpp:
