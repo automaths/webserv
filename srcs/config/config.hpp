746847db49027fd9641bb527a840d8f8df19cbcd
nimrodds <nsartral@student.42.fr>
2022-09-28 19:41:31 +0200

    refactoring

--- srcs/config/config.hpp
+++ srcs/config/config.hpp
@@ -48,9 +48,9 @@ class Config {
     ~Config(){}
 
     Config(std::string str){
+
         _file = str;
         check_brackets();
-
         while (((_file.find("http", 0) >= 0) && (_file[_file.find_first_not_of(" \t\v\n\r\f", _file.find("http", 0) + 4)] == '{')) || \
             ((_file.find("server", 0) >= 0) && (_file[_file.find_first_not_of(" \t\v\n\r\f", _file.find("server", 0) + 6)] == '{')))
         {
@@ -58,55 +58,47 @@ class Config {
             {
                 while ((_file.find("http") > 0 && (_file[_file.find_first_not_of(" \t\v\n\r\f", 4)] != '{')))
                     _file.erase(0, 1);
-                std::string::iterator it = _file.begin();
-                while (*it != '{')
-                    ++it;
-                ++it;
-                int n = 1;
-                while (it != _file.end() && n > 0)
-                {
-                    if (*it == '{')
-                        ++n;
-                    if (*it == '}')
-                        --n;
-                    ++it;
-                }
-                _chunks.push_back(_file.substr(0, it - _file.begin()));
-                for (unsigned int i = 0; i < it - _file.begin(); ++i)
+                std::string chunk = chunking();
+                _chunks.push_back(chunk);
+                for (unsigned int i = 0; i < chunk.size(); ++i)
                     _file.erase(0, 1);
             }
             else
             {
                 while ((_file.find("server") > 0 && (_file[_file.find_first_not_of(" \t\v\n\r\f", 4)] != '{')))
                     _file.erase(0, 1);
-                std::string::iterator it = _file.begin();
-                while (*it != '{')
-                    ++it;
-                ++it;
-                int n = 1;
-                while (it != _file.end() && n > 0)
-                {
-                    if (*it == '{')
-                        ++n;
-                    if (*it == '}')
-                        --n;
-                    ++it;
-                }
-                _chunks.push_back(_file.substr(0, it - _file.begin()));
-                for (unsigned int i = 0; i < it - _file.begin(); ++i)
+                std::string chunk = chunking();
+                _chunks.push_back(chunk);
+                for (unsigned int i = 0; i < chunk.size(); ++i)
                     _file.erase(0, 1);
             }
         }
-
-        int t = 1;
-        for (std::list<std::string>::iterator it = _chunks.begin(); it != _chunks.end(); ++it)
-        {
-            std::cout << "THE CHUNK " << t++ << " IS: \n\n" << std::endl;
-            std::cout << *it << std::endl;
-        }
+        // int n = 1;
+        // std::ofstream ofs;
+        // ofs.open("config_result.txt");
+        // for (std::list<std::string>::iterator tmp = _chunks.begin(); tmp != _chunks.end(); ++tmp)
+        //     ofs << "\nTHE CHUNK " << n++ << " IS: \n\n" << *tmp << std::endl;
     }
 
     void check_brackets();
+
+    std::string chunking() {
+
+        std::string::iterator it = _file.begin();
+        while (*it != '{')
+            ++it;
+        ++it;
+        int n = 1;
+        while (it != _file.end() && n > 0)
+        {
+            if (*it == '{')
+                ++n;
+            if (*it == '}')
+                --n;
+            ++it;
+        }
+        return (_file.substr(0, it - _file.begin()));
+    }
 
     private:
 
