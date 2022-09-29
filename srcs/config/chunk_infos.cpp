41af8b2eefd72aa0533abfc9d57dec2442f9e91f
nimrodds <nsartral@student.42.fr>
2022-09-29 22:33:18 +0200

    body max & root

--- srcs/config/chunk_infos.cpp
+++ srcs/config/chunk_infos.cpp
@@ -1,7 +1,7 @@
 #include "library.hpp"
 #include "chunk_infos.hpp"
 
-void Chunk_Infos::extract_host_port(){
+void Chunk_Infos::extract_address_port(){
     if (_chunk.find("listen ", 0) != std::string::npos)
     {
         std::string listen_dir = _chunk.substr(_chunk.find("listen ", 0), _chunk.find_first_of(';', _chunk.find("listen ", 0)) - _chunk.find("listen ", 0));
@@ -62,3 +62,59 @@ void Chunk_Infos::extract_server_name() {
     }
 }
 
+void Chunk_Infos::extract_default_error_pages() {
+    while (_chunk.find("error_page ", 0) != std::string::npos)
+    {
+        std::list<std::string> number;
+        std::string path;
+
+        std::string error_page_dir = _chunk.substr(_chunk.find("error_page ", 0), _chunk.find_first_of(';', _chunk.find("error_page ", 0)) - _chunk.find("error_page ", 0));
+        //little trick here to stop while loop
+        _chunk.erase(_chunk.find("error_page ", 0), 1);
+        error_page_dir.erase(0, error_page_dir.find_first_of("error_page ") + 11);
+        while (error_page_dir.find_first_of(" \t\v\n\r\f", 0) == 0)
+            error_page_dir.erase(0, 1);
+        while (error_page_dir.size() != 0)
+        {  
+            if (error_page_dir.find_first_of(" \t\v\n\r\f", 0) != std::string::npos)
+            {
+                number.push_back(error_page_dir.substr(0, error_page_dir.find_first_of(" \t\v\n\r\f", 0)));
+                error_page_dir.erase(0, error_page_dir.find_first_of(" \t\v\n\r\f", 0));
+            }
+            else
+            {
+                number.push_back(error_page_dir.substr(0, error_page_dir.size()));
+                error_page_dir.erase(0, error_page_dir.size());
+            }
+            while(error_page_dir.find_first_of(" \t\v\n\r\f", 0) == 0)
+                error_page_dir.erase(0, 1);
+        }
+        path = number.back();
+        number.pop_back();
+        _default_error_pages.push_back(std::make_pair(number, path));
+    }
+}
+
+void Chunk_Infos::extract_client_body_buffer_size() {
+    if (_chunk.find("client_body_buffer_size ", 0) != std::string::npos)
+    {
+        std::string body_max = _chunk.substr(_chunk.find("client_body_buffer_size ", 0), _chunk.find_first_of(';', _chunk.find("client_body_buffer_size ", 0)) - _chunk.find("client_body_buffer_size ", 0));
+        body_max.erase(0, body_max.find_first_of("client_body_buffer_size ") + 24);
+        _client_body_buffer_size = body_max.substr(body_max.find_first_not_of("\t\v\n\r\f ", 0), body_max.find_first_of("\t\v\n\r\f ", body_max.find_first_not_of("\t\v\n\r\f ", 0)));
+
+    }
+    else
+        _client_body_buffer_size = "";
+}
+
+void Chunk_Infos::extract_root() {
+    if (_chunk.find("root ", 0) != std::string::npos)
+    {
+        std::string root = _chunk.substr(_chunk.find("root ", 0), _chunk.find_first_of(';', _chunk.find("root ", 0)) - _chunk.find("root ", 0));
+        root.erase(0, root.find_first_of("root ") + 5);
+        _root = root.substr(root.find_first_not_of("\t\v\n\r\f ", 0), root.find_first_of("\t\v\n\r\f ", root.find_first_not_of("\t\v\n\r\f ", 0)));
+
+    }
+    else
+        _root = "html";
+}
--- srcs/config/chunk_infos.hpp
+++ srcs/config/chunk_infos.hpp
@@ -11,63 +11,39 @@ class Chunk_Infos {
 
     Chunk_Infos(std::string str){
         _chunk = str;
-        extract_host_port();
+        extract_address_port();
         extract_server_name();
         extract_default_error_pages();
+        extract_client_body_buffer_size();
+        extract_root();
 
         print_infos();
     }
 
-    void extract_default_error_pages() {
-        while (_chunk.find("error_page ", 0) != std::string::npos)
-        {
-            std::list<std::string> number;
-            std::string path;
-
-            std::string error_page_dir = _chunk.substr(_chunk.find("error_page ", 0), _chunk.find_first_of(';', _chunk.find("error_page ", 0)) - _chunk.find("error_page ", 0));
-            //little trick here to stop while loop
-            _chunk.erase(_chunk.find("error_page ", 0), 1);
-            error_page_dir.erase(0, error_page_dir.find_first_of("error_page ") + 11);
-            while (error_page_dir.find_first_of(" \t\v\n\r\f", 0) == 0)
-                error_page_dir.erase(0, 1);
-            while (error_page_dir.size() != 0)
-            {  
-                if (error_page_dir.find_first_of(" \t\v\n\r\f", 0) != std::string::npos)
-                {
-                    number.push_back(error_page_dir.substr(0, error_page_dir.find_first_of(" \t\v\n\r\f", 0)));
-                    error_page_dir.erase(0, error_page_dir.find_first_of(" \t\v\n\r\f", 0));
-                }
-                else
-                {
-                    number.push_back(error_page_dir.substr(0, error_page_dir.size()));
-                    error_page_dir.erase(0, error_page_dir.size());
-                }
-                while(error_page_dir.find_first_of(" \t\v\n\r\f", 0) == 0)
-                    error_page_dir.erase(0, 1);
-            }
-            path = number.back();
-            number.pop_back();
-            _default_error_pages.push_back(std::make_pair(number, path));
-        }
-    }
 
 
     void print_infos() {
         std::cout << "the address is: " << _address << std::endl;
         std::cout << "the port is: " << _port << std::endl;
         for (std::list<std::string>::iterator it = _server_names.begin(); it != _server_names.end(); ++it)
-            std::cout << "Server_name : " << *it << std::endl;
+            std::cout << "server_name: " << *it << std::endl;
         for (std::list<std::pair<std::list<std::string>, std::string> >::iterator it = _default_error_pages.begin(); it != _default_error_pages.end(); ++it)
         {
-            std::cout << "Error pages : " << std::endl;
+            std::cout << "error pages: " << std::endl;
             for (std::list<std::string>::iterator ite = it->first.begin(); ite != it->first.end(); ++ite)
-                std::cout << "  Error number: " << *ite << std::endl;
-            std::cout << "  Path: " << it->second << std::endl;
+                std::cout << "  error number: " << *ite << std::endl;
+            std::cout << "  path: " << it->second << std::endl;
         }
+        std::cout << "body max: " << _client_body_buffer_size << std::endl;
+        std::cout << "root: " << _root << std::endl;
     }
 
-    void extract_host_port();
+    void extract_address_port();
     void extract_server_name();
+    void extract_default_error_pages();
+    void extract_client_body_buffer_size();
+    void extract_root();
+    // void extract_location() {}
 
     private:
 
@@ -77,7 +53,8 @@ class Chunk_Infos {
     std::string _port;
     std::list<std::string> _server_names;
     std::list<std::pair<std::list<std::string>, std::string> > _default_error_pages;
-    // std::string client_body_max_size;
+    std::string _client_body_buffer_size;
+    std::string _root;
     // std::list<Location_Infos> locations;
     // std::list<std::string> cgi;
 
