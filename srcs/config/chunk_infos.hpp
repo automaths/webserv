41e771fd490ec0e1574ee9ecc6339d3f7b3fb71d
nimrodds <nsartral@student.42.fr>
2022-09-29 23:48:38 +0200

    location block

--- srcs/config/chunk_infos.hpp
+++ srcs/config/chunk_infos.hpp
@@ -16,9 +16,35 @@ class Chunk_Infos {
         extract_default_error_pages();
         extract_client_body_buffer_size();
         extract_root();
+        extract_location();
 
-        print_infos();
+        // print_infos();
     }
+
+    void extract_address_port();
+    void extract_server_name();
+    void extract_default_error_pages();
+    void extract_client_body_buffer_size();
+    void extract_root();
+
+    // void extract_add_header() {}
+
+    void extract_location() {
+
+        std::list<std::string> location_blocks;
+        while ((_chunk.find("location ", 0) >= 0) && (_chunk[_chunk.find_first_not_of(" \t\v\n\r\f", _chunk.find("location", 0) + 8)] == '/'))
+        {
+            location_blocks.push_back(_chunk.substr(_chunk.find("location", 0), _chunk.find_first_of('}', _chunk.find("location", 0)) - _chunk.find("location", 0) + 1));
+            //little trick here to stop while loop
+            _chunk.erase(_chunk.find("location ", 0), 1);
+        }
+        //then reiterate to the same parsing than server
+    }
+
+
+
+
+
 
 
 
@@ -38,13 +64,6 @@ class Chunk_Infos {
         std::cout << "root: " << _root << std::endl;
     }
 
-    void extract_address_port();
-    void extract_server_name();
-    void extract_default_error_pages();
-    void extract_client_body_buffer_size();
-    void extract_root();
-    // void extract_location() {}
-
     private:
 
     std::string _chunk;
@@ -55,6 +74,9 @@ class Chunk_Infos {
     std::list<std::pair<std::list<std::string>, std::string> > _default_error_pages;
     std::string _client_body_buffer_size;
     std::string _root;
+
+
+    
     // std::list<Location_Infos> locations;
     // std::list<std::string> cgi;
 
--- srcs/config/obj/config.d
+++
@@ -1,7 +0,0 @@
-obj/config.o: config.cpp config.hpp library.hpp chunk_infos.hpp
-
-config.hpp:
-
-library.hpp:
-
-chunk_infos.hpp:
