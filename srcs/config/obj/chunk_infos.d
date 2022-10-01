4762a077a6ec908af0f5911c2f9cd98586dd451b
nimrodds <nsartral@student.42.fr>
2022-10-01 21:06:35 +0200

    implementing class recursion location

--- srcs/config/obj/chunk_infos.d
+++
@@ -1,8 +0,0 @@
-obj/chunk_infos.o: chunk_infos.cpp library.hpp chunk_infos.hpp \
-  location_infos.hpp
-
-library.hpp:
-
-chunk_infos.hpp:
-
-location_infos.hpp:
--- srcs/config/obj/location_infos.d
+++
@@ -1,5 +0,0 @@
-obj/location_infos.o: location_infos.cpp library.hpp location_infos.hpp
-
-library.hpp:
-
-location_infos.hpp:
