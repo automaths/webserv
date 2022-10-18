ebdf1f09fc7b01da6b17e2c603779a65682424be
automaths <nicolassartral@gmail.com>
2022-10-18 12:22:24 -0400

    fix new g++

--- srcs/config/scope_server.cpp
+++ srcs/config/scope_server.cpp
@@ -126,7 +126,10 @@ void ServerScope::extract_cgi(std::string cgi_dir) {
     for (std::vector<std::string>::iterator it = content.begin(); it != content.end(); ++it)
     {
         if (it + 1 != content.end())
-            _cgi.insert(std::make_pair(*(it), *(++it)));
+        {
+            std::string tmp = *it;
+            _cgi.insert(std::make_pair(tmp, *(++it)));
+        }
     }
 }
 
