17bd33774b9e9f2ad7241908d31f583759881ec7
Theodore NATON <tnaton@e1r10p3.clusters.42paris.fr>
2022-10-19 20:33:44 +0200

    mhm unclear if its perfect because it kinda wait forever if you dont give him hexa code afer \r\n so idk

--- www/default/cgi/form.php
+++
@@ -1,5 +0,0 @@
-<form action="/cgi/action.php" method="POST">
- <p>Votre nom : <input type="text" name="nom" /></p>
- <p>Votre âge : <input type="text" name="age" /></p>
- <p><input type="submit" value="OK"></p>
-</form>
--- www/default/cgi/script.py
+++
@@ -1,5 +0,0 @@
-a = 2000
-b = 21
-print(f"a = {a}")
-print(f"b = {b}")
-print(f"a + b = {a + b}")
